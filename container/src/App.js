d6e6a54687df11b9a3a74158a5279e6ae022437c
Vinci-da-Gama <kylingourmand@gmail.com>
2023-04-13 18:37:46 +1000

    s9 add header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import MarketingMiniApp from './miniapp-compos/MarketingMiniApp'
+import Header from './compos/Header'
 
 const App = () => {
   return (
-    <MarketingMiniApp />
+    <Router>
+      <Header />
+      <MarketingMiniApp />
+    </Router>
   )
 }
 
