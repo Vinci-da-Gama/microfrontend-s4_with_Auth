aa38ec8e43141ace31da8dde01229b3afe3a3080
Vinci-da-Gama <kylingourmand@gmail.com>
2023-04-21 19:41:00 +1000

    use Route replace <MarketingMiniApp />

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { BrowserRouter as BsRouter } from 'react-router-dom'
+import React, { lazy, Suspense } from 'react'
+import { BrowserRouter as BsRouter, Routes, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 
-import MarketingMiniApp from './miniapp-compos/MarketingMiniApp'
+// import MarketingMiniApp from './miniapp-compos/MarketingMiniApp'
+const MarketingMiniApp = lazy(() => import("./miniapp-compos/MarketingMiniApp"));
 import Header from './compos/Header'
 
 const history = createBrowserHistory();
@@ -17,7 +18,9 @@ const App = () => {
     <BsRouter>
       <StylesProvider generateClassName={generateClassName}>
         <Header />
-        <MarketingMiniApp browserHistory={ history } />
+        <Routes>
+          <Route path={'/*'} element={<MarketingMiniApp />} />
+        </Routes>
       </StylesProvider>
     </BsRouter>
   )
