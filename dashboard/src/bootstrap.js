0a79b0193b958cd14eec974a581f8f3e936b31a2
Vinci-da-Gama <kylingourmand@gmail.com>
2023-04-23 23:18:06 +1000

    try deploy dashboard

--- dashboard/src/bootstrap.js
+++ dashboard/src/bootstrap.js
@@ -4,7 +4,7 @@ import Dashboard from './components/Dashboard.vue';
 // Mount function to start up the app
 const mount = (el) => {
   const app = createApp(Dashboard);
-  app.mount(el);
+  app.mount(el)
 };
 
 // If we are in development and in isolation,
