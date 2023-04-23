5e82a1f261dfa54100f7de26ff4fc91812a4c373
Vinci-da-Gama <kylingourmand@gmail.com>
2023-04-23 23:22:01 +1000

    try dashboar again

--- dashboard/src/bootstrap.js
+++ dashboard/src/bootstrap.js
@@ -3,7 +3,7 @@ import Dashboard from './components/Dashboard.vue';
 
 // Mount function to start up the app
 const mount = (el) => {
-  const app = createApp(Dashboard);
+  const app = createApp(Dashboard)
   app.mount(el)
 };
 
